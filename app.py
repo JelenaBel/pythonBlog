c39557d5ff9dd9a2bfd0e2db50973efb52a6d429
Jelena <elena.belousova@gmail.com>
2021-11-18 01:26:04 +0200

    proc file

--- app.py
+++ app.py
@@ -3,20 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///catering.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Article(db.Model):
+class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), primary_key=False)
-    intro = db.Column(db.String(300), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    price = db.Column(db.Float(7), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    photo = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Article %r' % self.id
+        return f"<products {self.id}>"
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), primary_key=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), nullable=True)
+
+    def __repr__(self):
+        return f"<users {self.id}>"
 
 
 @app.route('/')
@@ -25,74 +35,52 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/menus')
+def menus():
+    return render_template("menus2.html")
+
+@app.route('/menusdetail')
+def menusdetail():
+    return render_template("menusdetail.html")
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    return render_template("signup.html")
+
+
+@app.route('/register', methods=['POST', 'GET'])
+
+def register():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        if request.form['password'] == request.form['passwordRepeat']:
+            password = request.form['password']
+        user = Users(name=name, email=email, password=password)
+
+        try:
+            db.session.add(user)
+            db.session.flush()
+            db.session.commit()
+            return redirect('/signup')
+        except:
+            db.session.rollback()
+            return "При регистрации произошла ошибка"
+
+    return render_template("register.html")
+
+
 @app.route('/about')
 def about():
     return render_template("about.html")
 
 
-@app.route('/create-article', methods=['POST', 'GET'])
-def create_article():
-    if request.method == "POST":
-        title = request.form['title']
-        intro = request.form['intro']
-        text = request.form['text']
-        article = Article(title=title, intro=intro, text=text)
-
-        try:
-            db.session.add(article)
-            db.session.commit()
-            return redirect('/posts')
-        except:
-            return "При добавлении статьи произошла ошибка"
-
-    else:
-        return render_template("create-article.html")
-
-
-@app.route('/posts')
-def posts():
-    articles = Article.query.order_by(Article.date.desc()).all()
-    return render_template("posts.html", articles=articles)
-
-
-@app.route('/posts/<int:id>')
-def post_detail(id):
-
-    article = Article.query.get(id)
-    return render_template("posts-detail.html", article=article)
-
-
-@app.route('/posts/<int:id>/delete')
-def post_delete(id):
-    article = Article.query.get_or_404(id)
-    try:
-        db.session.delete(article)
-        db.session.commit()
-        return redirect('/posts')
-    except:
-        return "При редактировании статьи произошла ошибка"
-    return render_template("posts-detail.html", article=article)
-
-
-@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
-def post_update(id):
-    articleForUpdate = Article.query.get(id)
-    if request.method == "POST":
-        articleForUpdate.title = request.form['title']
-        articleForUpdate.intro = request.form['intro']
-        articleForUpdate.text = request.form['text']
-
-
-        try:
-
-            db.session.commit()
-            return redirect('/posts')
-        except:
-            return "При добавлении статьи произошла ошибка"
-
-    else:
-        article = Article.query.get(id)
-        return render_template("post-update.html", article=article)
 
 
 if __name__ == "__main__":
